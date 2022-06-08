bb107ae5f3aa5521083b0c4c0a99f4707f778df4
Daniel <dherzog100@gmail.com>
2022-06-07 21:10:50 -0700

    set up local storage for the save button

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var saveButton = $(".saveBtn")
 
 saveButton.on('click' , function () {
+var timeId = $(this).parent().attr("id")
+var description = $(this).siblings(".description").val()
+  localStorage.setItem(timeId , description) 
+})
+  
+// const currentHour = new Date().getHours()
+const currentHour = 104
+    $('div[id^="block-"]').each (function(){
+    var timeId = $(this).attr("id")
+        var description = localStorage.getItem(timeId)
+        $(this).children(".description").val(description)
 
-console.log($(this).siblings(".description").val())
-    })
+        var divHour = +timeId.split("-")[1]
+        if (divHour<currentHour){
+            $(this).addClass("past")
+        } else if (divHour>currentHour){
+            $(this).addClass("future")
+        } else {
+            $(this).addClass("present")
+        }
+})
